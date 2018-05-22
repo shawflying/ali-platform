269358d4af491eb458d4a0165b04a99a5fc6f7d5
闫肖肖 <yanxiaoxiao@daoxila.com>
2018-05-22 09:45:38 +0800

    init

---
+++ yunfengdie-study/config.js
@@ -0,0 +1,7 @@
+export default {
+  pages: {
+    '/index.html': { context: { title: '首页' } },
+    '/detail.html': { context: { title: '详情页' } },
+    '/users/list.html': { context: { title: '用户列表页' } },
+  },
+};
